4eb4f622e97aaf6a709d57ed355e1a45288006f7
charper24 <colton.harper@huskers.unl.edu>
2021-04-09 18:42:41 -0500

    basic slider functionality

--- projects/react-tutorial/src/App.js
+++ projects/react-tutorial/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import GlobalStyle from './globalStyles';
+import Hero from './components/Hero';
+import {SliderData} from './data/SliderData';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Hero slides={SliderData}/>
+    </>
   );
 }
 

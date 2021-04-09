028019282bf89479718a1e71b176c9862c729239
Keith Tran <keith.tran@huskers.unl.edu>
2021-04-09 18:44:41 -0500

    pushed meatlab project

---
+++ projects/meat-lab/src/components/graph-section/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+// import { Link as LinkR } from 'react-router-dom'
+// import { Link as LinkS } from 'react-scroll'
+
+export const GraphContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    z-index: 1; 
+    width: 100%;
+    padding: 0 24px;
+    max-width: 1100px;
+`
+
+const GraphMeat = () => {
+    return (
+        <>
+            <h1>HIee</h1>
+            <GraphContainer>
+
+            </GraphContainer>
+        </>
+    )
+}
+
+export default GraphMeat
